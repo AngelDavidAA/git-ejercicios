4eaecd074fa12762d9d8489e5e743d2f2081a0f1
David <angeldavid-esting@outlook.com>
2021-11-26 13:21:53 -0600

    modifaciones a index.html e index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import  ReactDOM, { render }  from "react-dom";
+
+//componentes
+import App from "./App";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App></App>
+    </React.StrictMode>
+)
+
+document.getElementById('root')
